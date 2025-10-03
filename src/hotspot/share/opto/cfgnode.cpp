a3df8f10e830e1c7b179e9fe8cb6d478391ddfd8
Marc Chevalier <marc.chevalier@oracle.com>
2025-10-03 16:32:03 +0200

    more logging

--- src/hotspot/share/opto/cfgnode.cpp
+++ src/hotspot/share/opto/cfgnode.cpp
@@ -1319,9 +1319,28 @@ const Type* PhiNode::Value(PhaseGVN* phase) const {
   // It is not possible to see Type::BOTTOM values as phi inputs,
   // because the ciTypeFlow pre-pass produces verifier-quality types.
   const Type* ft = t->filter_speculative(_type);  // Worst case type
+#ifdef ASSERT
   const Type* ft_ = t->filter_speculative(ft);
   if (!Type::equals(ft, ft_)) {
     stringStream ss;
+
+    for (uint i = 1; i < req(); ++i) {
+      ss.print("in(%d): ", i);
+      if (r->in(i) && phase->type(r->in(i)) == Type::CONTROL) {
+        const Type* ti = phase->type(in(i));
+        ti->dump_on(&ss);
+      }
+      ss.print_cr("");
+    }
+
+    ss.print("t: ");
+    t->dump_on(&ss);
+    ss.print_cr("");
+
+    ss.print("_type: ");
+    _type->dump_on(&ss);
+    ss.print_cr("");
+
     ss.print("Filter once: ");
     ft->dump_on(&ss);
     ss.print_cr("");
@@ -1329,8 +1348,10 @@ const Type* PhiNode::Value(PhaseGVN* phase) const {
     ft_->dump_on(&ss);
     ss.print_cr("");
     tty->print("%s", ss.base());
+    tty->flush();
     assert(false, "Ventrebleu!");
   }
+#endif
 
 #ifdef ASSERT
   // The following logic has been moved into TypeOopPtr::filter.
